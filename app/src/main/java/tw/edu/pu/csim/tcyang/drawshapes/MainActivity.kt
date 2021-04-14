a290612f2fe5589bbe1ffdffe5b233d47fa0615c
tcyang <tcyang@gm.pu.edu.tw>
2021-04-14 08:51:21 +0800

    按鈕及觸控監聽

--- app/src/main/java/tw/edu/pu/csim/tcyang/drawshapes/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/drawshapes/MainActivity.kt
@@ -1,11 +1,37 @@
 package tw.edu.pu.csim.tcyang.drawshapes
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity(),
+    View.OnClickListener, View.OnTouchListener{
+
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener(this)
+        draw_view.setOnTouchListener(this)
+
+        draw_view.setStrokeWidth(70.0f) //畫筆粗細
+        draw_view.setColor(Color.WHITE) //畫筆顏色
+        draw_view.setBackgroundColor(Color.BLACK) //背景顏色
+    }
+
+    override fun onClick(p0: View?) {
+        draw_view.clearCanvas()  //清除繪圖區
+    }
+
+    override fun onTouch(p0: View?, event: MotionEvent): Boolean {
+        draw_view.onTouchEvent(event)
+        if (event.action == MotionEvent.ACTION_UP){
+            Toast.makeText(this, "手指彈起",Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
